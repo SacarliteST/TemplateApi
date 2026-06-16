7c4e02f9a4d66dcc04927b78cc213876024030c5
vladislav.bokovoi <vladislav.bokovoi@monopoly.su>
2026-06-16 23:56:23 +0300

    Фикс скриптов

--- AddMigrationForSqlite.ps1
+++ AddMigrationForSqlite.ps1
@@ -1,3 +1,3 @@
 $MigrationName = Read-Host "Enter Migration name "
-dotnet ef migrations add $MigrationName -c SqliteDbContext -s Host -p Data -o /Core/Migrations/SQLite -v -- --CreateMigrationOnly
+dotnet ef migrations add $MigrationName -c SqliteDbContext -s Host -p Data -o ./Core/Migrations/SqlLite -v -- --CreateMigrationOnly
 Read-Host -Prompt "Press Enter to exit"
